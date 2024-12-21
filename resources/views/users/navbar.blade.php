acf9bc35dc5654c87095d0f0fa21d0ea932eef56
lamhotsimamora <lamhotsimamora36@gmail.com>
2024-12-21 20:15:50 +0700

    +

--- resources/views/users/navbar.blade.php
+++ resources/views/users/navbar.blade.php
@@ -1,7 +1,7 @@
  <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="." class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="http://103.178.153.220/public/storage/logo.webp" class="h-8" alt="" />
+                <img src="https://derania.com/public/storage/logo.webp" class="h-8" alt="" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Deratek</span>
             </a>
             <button data-collapse-toggle="navbar-default" type="button"
@@ -18,7 +18,7 @@
                 <ul
                     class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
-                        <a href="http://derania.com/public/index.php/users"
+                        <a href="https://derania.com/public/index.php/users"
                             class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                             aria-current="page">Home</a>
                     </li>
